4f6d0612189ef24f213562c7be9f50d5f8e33ecf
18wh1a1215 <69303964+18wh1a1215@users.noreply.github.com>
2020-08-29 18:16:08 +0530

    Add files via upload

---
+++ CN&WT Lab/CN Experiments/Experiment1/bit_stuffing.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main() {
+int i=0,count=0,n;
+char a[50];
+printf("Enter the size of the frame");
+scanf("%d",&n);
+printf("Enter the input frame :");
+scanf("%s",&a);
+printf("After stuffing \n");
+printf("01111110");
+if(a[i]=0;i<n;i++) {
+if(a[i]=='1')
+count++;
+printf("0");
+count=0;
+}
+}
+printf("01111110");
+return 0;
+}
